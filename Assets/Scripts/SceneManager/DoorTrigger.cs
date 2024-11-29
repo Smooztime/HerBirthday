1c6360c785badace7307d7e87f45737298e26e49
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2024-11-28 18:40:04 -0700

    Level Progress bar display add

--- Assets/Scripts/SceneManager/DoorTrigger.cs
+++ Assets/Scripts/SceneManager/DoorTrigger.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,24 +5,28 @@ public class DoorTrigger : MonoBehaviour
 {
     public UnityEvent doorTrigger;
     public UnityEvent doorTriggerExit;
-    private bool canOpen =true;
+    private bool canOpen = true;
     [SerializeField] int doorID = 0;
     public int DoorId()
     { return doorID; }
+    private bool isActived = false;
 
-private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (canOpen && !other.GetComponent<PlayerController>())
+        if (canOpen && other.GetComponent<PlayerController>())
         {
+            //progress bar in level
+            MenuManager.Instance.activedRoom++;
+
             doorTrigger?.Invoke();
             Debug.Log("player enter room");
-            if (doorID ==4)
+            if (doorID == 4)
             {
                 SoundManager.Instance.PlaySFX("Fight", 1f);
             }
         }
     }
-    
+
     private void OnTriggerExit(Collider other)
     {
         if (!other.GetComponent<PlayerController>())
